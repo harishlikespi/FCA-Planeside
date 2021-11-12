dc361cc32de5a922488bcd83a14ae38fccf74d61
Shrey Varma <66108366+Shrey-Varma@users.noreply.github.com>
2021-11-11 19:21:39 -0800

    Create GPS_info.ino
    initial commit - has most important sensor info - long/lat, speed, heading, altitude, time

---
+++ embedded/GPS_info.ino
@@ -0,0 +1,83 @@
+#include <Wire.h> //Needed for I2C to GNSS
+
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
+SFE_UBLOX_GNSS myGNSS;
+
+long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial); //Wait for user to open terminal
+
+  Wire.begin();
+
+  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  {
+    Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+
+   myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  //myGNSS.saveConfiguration();        //Optional: Save the current settings to flash and BBR
+}
+
+void loop()
+{
+  //Query module only every second. Doing it more often will just cause I2C traffic.
+  //The module only responds when a new position is available
+  if (millis() - lastTime > 1000)
+  {
+    lastTime = millis(); //Update the timer
+    
+    long latitude = myGNSS.getLatitude();
+    Serial.print(F("Lat: "));
+    Serial.print(latitude/1000000);
+
+    long longitude = myGNSS.getLongitude();
+    Serial.print(F(" Long: "));
+    Serial.print(longitude/1000000);
+
+    long speed = myGNSS.getGroundSpeed();
+    Serial.print(F(" Speed: "));
+    Serial.print(speed);
+    Serial.print(F(" (mm/s)"));
+
+    long heading = myGNSS.getHeading();
+    Serial.print(F(" Heading: "));
+    Serial.print(heading);
+    Serial.print(F(" (degrees * 10^-5)"));
+
+    long altitude = myGNSS.getAltitude();
+    Serial.print(F(" Alt: "));
+    Serial.print(altitude);
+    Serial.print(F(" (mm)"));
+
+    Serial.println();
+    Serial.print(myGNSS.getYear());
+    Serial.print("-");
+    Serial.print(myGNSS.getMonth());
+    Serial.print("-");
+    Serial.print(myGNSS.getDay());
+    Serial.print(" ");
+    Serial.print(myGNSS.getHour());
+    Serial.print(":");
+    Serial.print(myGNSS.getMinute());
+    Serial.print(":");
+    Serial.print(myGNSS.getSecond());
+
+    Serial.print("  Time is ");
+    if (myGNSS.getTimeValid() == false)
+    {
+      Serial.print("not ");
+    }
+    Serial.print("valid  Date is ");
+    if (myGNSS.getDateValid() == false)
+    {
+      Serial.print("not ");
+    }
+    Serial.print("valid");
+
+    Serial.println();
+  }
+}
