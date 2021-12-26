fd1987ed040629e3d63b2569795e980a635aee69
halantrann <90490493+halantrann@users.noreply.github.com>
2021-12-26 15:04:01 -0800

    Update GPS_sensor.ino

--- embedded/GPS_sensor.ino
+++ embedded/GPS_sensor.ino
@@ -1,16 +1,24 @@
 #include <Wire.h>
 #include <SparkFun_Ublox_Arduino_Library.h> //http://librarymanager/All#SparkFun_Ublox_GPS
+#include <SoftwareSerial.h> 
+
+#define GPSTX A4 // output 
+#define GPSRX A5 // input 
+#define GPSBaud 9600 // baud rate for gps
+#define Serial_Monitor_Baud 115200 // baud rate for serial monitor
+
+SoftwareSerial GPSserial(GPSRX, GPSTX);  
 
 // GPS object
 SFE_UBLOX_GPS myGPS; 
 
-void setup() {
+void setup(){
 
   Serial.begin(115200);
 
   // waiting for Serial to connect
   while(!Serial) yield(); 
-  
+
   Wire.begin();
 
   // in the case that GPS fails to initialize
@@ -21,6 +29,9 @@ void setup() {
 
   myGPS.setI2COutput(COM_TYPE_UBX); 
   myGPS.saveConfiguration(); 
+
+  Serial.begin(Serial_Monitor_Baud);
+  GPSserial.begin(GPSBaud); 
 
 }
 
@@ -49,6 +60,9 @@ void loop() {
       float time = myGPS.getTimeValid();
       Serial.println("Time: ");
       Serial.print(time);   
+
+      while (GPSserial.available() > 0)
+      Serial.write(GPSserial.read());
 
     }
 }
